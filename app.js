4682a4b20041fc2b4ec977c6897dfb653c2d4730
Alexis1985 <alexis.duranmachuca@gmail.com>
2022-04-16 21:07:52 -0400

    Adding prices and event delegation

--- app.js
+++ app.js
@@ -1,45 +1,83 @@
-const template = document.querySelector('#template');
 const fragment = document.createDocumentFragment();
-const buttons = document.querySelectorAll('.card-btn');
+const template = document.querySelector('#template');
 const list = document.querySelector('.list');
+const templateFooter = document.querySelector('#templateFooter');
+const footer = document.querySelector('#footer');
 
-const cart = []; // Carrito
+// const addBtn = document.querySelector('.addBtn');
+// const removeBtn = document.querySelector('.removeBtn');
 
-//Add item to the cart then print
-const addItem = (e) => {
+const cart = [];
+
+document.addEventListener("click", e => {
+
+    if (e.target.matches('.card .card-btn') || e.target.matches('.addBtn')){
+        console.log('Add new item to cart')
+        addItemCart(e);
+    } else if (e.target.matches('.removeBtn')){
+        delItemCart(e);
+    };
+});
+
+const addItemCart = (e) => {
+    
     const item = {
-        name: e.target.value,
-        amount: 1
+        name: e.target.dataset.id,
+        price: parseInt(e.target.dataset.price),
+        amount: 1,
     }
 
     const exist = cart.findIndex( (fItem) => fItem.name === item.name)
     
     if(exist === -1) {
-        cart.push(item);
+            cart.push(item);
     }else {
-        cart[exist].amount++;
-    }
+            cart[exist].amount++;
+        }
+    
+    printCart();
+};  
 
+const delItemCart = (e) => {
+    const index = cart.findIndex( (item) => item.name === e.target.dataset.id)
+    
+    cart[index].amount--;
+    
+    if (cart[index].amount === 0){
+        cart.splice(index, 1);
+    }
     printCart();
 };
 
-// Print the cart's item on the list element
 const printCart = () => {
     
-    list.textContent = " "; // Limpiamos la lista
+    list.textContent = " "; 
+
     cart.forEach( (item) => {
-        const clone = template.content.firstElementChild.cloneNode(true);
+        const cloneTemp = template.content.cloneNode(true);
         
-        clone.querySelector('.itemName').textContent = item.name;
-        clone.querySelector('.itemAmount').textContent = item.amount;
+        cloneTemp.querySelector('.itemAmount').textContent = item.amount;
+        cloneTemp.querySelector('.itemName').textContent = item.name;
+        cloneTemp.querySelector('.totalSection .totalPrice span').textContent= 
+                    item.amount * item.price
 
-        fragment.appendChild(clone);
-        list.appendChild(fragment);
+        //Add id and price to add/remove bttns
+        cloneTemp.querySelector('.addBtn').dataset.id = item.name;
+        cloneTemp.querySelector('.addBtn').dataset.price = item.price
+        cloneTemp.querySelector('.removeBtn').dataset.id = item.name;
+        cloneTemp.querySelector('.removeBtn').dataset.price = item.price;
+
+        // suma += total;
+        fragment.appendChild(cloneTemp);
     } );
-
-
+    
+    const total = cart.reduce( (acc, current) => acc + current.amount * current.price, 0)
+    
+    if (total !== 0) {
+        const cloneTempF = templateFooter.content.cloneNode(true);
+        cloneTempF.querySelector('.total span').textContent = total;
+        fragment.appendChild(cloneTempF);
+    }
+    list.appendChild(fragment);
+    // console.log(cart)
 };
-
-buttons.forEach( (btn) => btn.addEventListener("click", addItem ))
-
-// probar con otros metodos el array
